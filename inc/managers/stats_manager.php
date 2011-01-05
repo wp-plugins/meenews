77e200ab9a6406ffa9eea11ea00f97d22a0c16c6
meenews <meenews@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-05 15:24:02 +0000

    de
    git-svn-id: https://plugins.svn.wordpress.org/meenews/trunk@329060 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/managers/stats_manager.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$pageinfo = array('full_name' => __('Statistics',"meenews"), 'optionname'=>'stats', 'child'=>true, 'filename' => basename(__FILE__),
-"tpl_needed" => array("home" => "blanc.html","HEADER" => "header.html") );
-
-$options = array();
-
-$options_page = new MeeStats($options, $pageinfo);
